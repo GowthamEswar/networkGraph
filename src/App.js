2b03b6a7d341e00a38e34faaf7f578a5e136cfcb
gowtham <gowtham.e@techjays.com>
2023-05-29 21:57:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import NetworkChart from './components/NetworkChart';
+
+function App() {
+  return (
+    <div className="App">
+      <NetworkChart />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NetworkChart.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react'
+import Graph from 'react-vis-network-graph'
+import { edges, nodes, Sheet1 } from './data'
+
+export default function NetworkChart() {
+    var options = {
+        nodes: {
+            shape: "dot",
+            scaling: {
+                min: 10,
+                max: 30,
+                label: {
+                    min: 8,
+                    max: 30,
+                    drawThreshold: 12,
+                    maxVisible: 20
+                }
+            },
+            font: {
+                size: 12,
+                face: "Tahoma"
+            }
+        },
+        edges: {
+            width: 0.15,
+            color: { inherit: "from" },
+            smooth: {
+                type: "continuous"
+            }
+        },
+        physics: false,
+        interaction: {
+            navigationButtons: true,
+            tooltipDelay: 200,
+            hideEdgesOnDrag: true,
+            hideEdgesOnZoom: true
+        },
+        height: "900px"
+    }
+
+    var data = { nodes: nodes, edges: edges }
+    return (
+        <div className='container'>
+            <Graph
+                graph={data}
+                options={options}
+            />
+        </div>
+    )
+}
